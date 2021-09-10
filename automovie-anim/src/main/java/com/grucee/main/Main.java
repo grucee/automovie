d79162f2c80295b8dd16b934bb1f2337c6523b00
grucee <546401099@qq.com>
2021-09-10 21:43:20 +0800

    cant run

--- automovie-anim/src/main/java/com/grucee/main/Main.java
+++ automovie-anim/src/main/java/com/grucee/main/Main.java
@@ -28,9 +28,12 @@ public class Main {
         AmScene scene = new AmScene(sceneWidth, sceneHeight);
 
         //step-2:构造元素
-        AmRect rect = new AmRect(200, 200);
-        rect.setFill(Color.YELLOW);
-        rect.setStroke(10.0f, Color.BLUE);
+        AmRect lRect = new AmRect(200, 200);
+//        lRect.setFill(Color.YELLOW);
+//        lRect.setStroke(10.0f, Color.BLUE);
+        AmRect rRect = new AmRect(200, 200);
+        AmRect uRect = new AmRect(200, 200);
+        AmRect dRect = new AmRect(200, 200);
 
         AmPoint point = new AmPoint();
         point.setFill(Color.RED);
@@ -38,7 +41,10 @@ public class Main {
         AmSVG svg = new AmSVG(inputSvgPath);
 
         //step-3: 向场景中添加元素
-        scene.add(rect);
+        scene.add(lRect);
+        scene.add(rRect);
+        scene.add(uRect);
+        scene.add(dRect);
         scene.add(point);
         scene.add(svg);
 
@@ -49,8 +55,12 @@ public class Main {
         AmMovieOutput output = new AmMovieOutput(sceneWidth, sceneHeight, outputMoviePath);
         output.start();
         for (int i = 0; i < 100; i++) {
-            rect.translate(1, 1);
-            svg.translate(2, 2);
+            lRect.translate(-10, 0);
+            rRect.translate(10, 0);
+            uRect.translate(0, -10);
+            dRect.translate(0, 10);
+
+            svg.translate(10, 0);
             BufferedImage sceneImage = render.render(scene);
             if (i == 0) {
                 output.writeFrame(sceneImage, outputImagePath);
